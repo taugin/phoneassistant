25780530c5c8e8012a2403b7015d29aa40248dea
liuzhao.wei <liuzhao.wei@chukong-inc.com>
2014-12-12 18:04:36 +0800

    1. Add barcode scan 2. Add webserver settings modified:   PhoneAssistantApp/AndroidManifest.xml new file:   PhoneAssistantApp/res/drawable-xhdpi/barcodescan.png new file:   PhoneAssistantApp/res/drawable-xhdpi/websettings.png new file:   PhoneAssistantApp/res/layout/barcode_layout.xml modified:   PhoneAssistantApp/res/layout/capture.xml modified:   PhoneAssistantApp/res/values-zh-rCN/strings.xml modified:   PhoneAssistantApp/res/values/colors.xml new file:   PhoneAssistantApp/res/values/ids.xml modified:   PhoneAssistantApp/res/values/strings.xml new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/AmbientLightManager.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/AutoFocusManager.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/BarCodeScanActivity.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/CameraConfigurationManager.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/CameraManager.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/CaptureActivity.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/CaptureActivityHandler.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/DecodeFormatManager.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/DecodeHandler.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/DecodeHintManager.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/DecodeThread.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/FrontLightMode.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/InactivityTimer.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/Intents.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/PreviewCallback.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/ViewfinderResultPointCallback.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/ViewfinderView.java new file:   PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/ZXingPreferences.java

---
+++ PhoneAssistantApp/src/com/android/phoneassistant/zxing/activity/BarCodeScanActivity.java
@@ -0,0 +1,119 @@
+package com.android.phoneassistant.zxing.activity;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.phoneassistant.R;
+import com.android.phoneassistant.util.Log;
+import com.google.zxing.client.result.ParsedResultType;
+
+public class BarCodeScanActivity extends Activity {
+
+    private static final int REQ_CAPTURE = 0x0001;
+
+    private String lastResult;
+    private ParsedResultType mResultType;
+    private String mType;
+    private String mResult;
+
+    private TextView mScanType;
+    private TextView mScanResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.barcode_layout);
+        mScanType = (TextView) findViewById(R.id.type);
+        mScanResult = (TextView) findViewById(R.id.result);
+        toCaptureActivity();
+    }
+
+    private void toCaptureActivity() {
+        try {
+            Intent intent = new Intent(this, CaptureActivity.class);
+            intent.setAction(Intents.Scan.ACTION);
+            startActivityForResult(intent, REQ_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onScan(View view) {
+        toCaptureActivity();
+    }
+
+    public void onCopy(View view) {
+        copy2Clipboard(mResult);
+    }
+
+    public void onOpen(View view) {
+        if (!TextUtils.isEmpty(mResult)) {
+            toBrowserActivity(mResult);
+        }
+    }
+    private void toBrowserActivity(String text) {
+        Uri uri = null;
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (mResultType == ParsedResultType.TEL) {
+            uri = Uri.parse("tel:" + text);
+        } else if (mResultType == ParsedResultType.URI) {
+            uri = Uri.parse(text);
+        } else if (mResultType == ParsedResultType.SMS) {
+            int index = text.indexOf("\n");
+            String phone = text.substring(0, index);
+            String body = text.substring(index);
+            uri = Uri.parse("smsto:" + phone);
+            intent.putExtra("sms_body", body);
+        }
+        if (uri != null) {
+            intent.setData(uri);
+            startActivity(intent);
+        }
+    }
+
+    private void copy2Clipboard(String text) {
+        ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        cm.setText(text);
+        Toast.makeText(this, "Copy Success", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQ_CAPTURE) {
+            if (resultCode == RESULT_OK) {
+                String result = data.getStringExtra(Intents.Scan.RESULT);
+                ParsedResultType type = ParsedResultType.values()[data
+                        .getIntExtra(Intents.Scan.RESULT_TYPE,
+                                ParsedResultType.TEXT.ordinal())];
+                mResultType = type;
+                mResult = result;
+                mType = type.name();
+                mScanType.setText(type.name());
+                mScanResult.setText(result);
+                Log.d(Log.TAG, "result : " + result);
+                boolean isShow = false;
+                try {
+                    if (type == ParsedResultType.URI) {
+                        // toBrowserActivity(result);
+                    } else {
+                        isShow = true;
+                    }
+                } catch (ActivityNotFoundException e) {
+                    isShow = true;
+                } finally {
+                    lastResult = result;
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
